1cbce3f2e3752056c66bfa68e1994dee423304a3
Hendy Irawan <hendy@soluvas.com>
2012-07-20 09:11:17 +0700

    proper assembly, OSGi bundle

--- fbcli/fbcli.sh
+++ fbcli/fbcli.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-# Must run first: mvn package dependency:copy-dependencies
-java -cp 'target/dependency/*:target/fbcli-1.0.0-SNAPSHOT.jar' org.jboss.weld.environment.se.StartMain "$@"
-
+java -cp 'lib/*.jar' org.jboss.weld.environment.se.StartMain "$@"
